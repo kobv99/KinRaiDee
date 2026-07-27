54bf5637849cd6e20aa4a58543ee85e6d09b6eca
kobv99 <70331345+kobv99@users.noreply.github.com>
2026-07-27 22:01:27 +0700

    fix(test): dispose GoRouter with app lifecycle

--- apps/mobile/lib/app/app.dart
+++ apps/mobile/lib/app/app.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-import 'theme/app_theme.dart';
 import 'router/app_router.dart';
+import 'theme/app_theme.dart';
 
-class KinRaiDeeApp extends StatelessWidget {
+class KinRaiDeeApp extends StatefulWidget {
   const KinRaiDeeApp({super.key});
+
+  @override
+  State<KinRaiDeeApp> createState() => _KinRaiDeeAppState();
+}
+
+class _KinRaiDeeAppState extends State<KinRaiDeeApp> {
+  late final GoRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _router = createAppRouter();
+  }
+
+  @override
+  void dispose() {
+    _router.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'KinRaiDee',
-
       debugShowCheckedModeBanner: false,
-
       theme: AppTheme.light,
-
-      routerConfig: appRouter,
+      routerConfig: _router,
     );
   }
 }
