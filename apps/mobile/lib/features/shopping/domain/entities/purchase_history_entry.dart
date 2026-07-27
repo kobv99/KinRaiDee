d58190b7a472b9eaa835fcbd2b847a65c507b2ee
kobv99 <70331345+kobv99@users.noreply.github.com>
2026-07-27 23:37:16 +0700

    feat(shopping): add purchase history projection model

---
+++ apps/mobile/lib/features/shopping/domain/entities/purchase_history_entry.dart
@@ -0,0 +1,34 @@
+class PurchaseHistoryEntry {
+  PurchaseHistoryEntry({
+    this.metadataVersion = currentPurchaseHistoryEntryVersion,
+    required this.id,
+    required this.purchasedAt,
+    required this.ingredientId,
+    required this.canonicalIngredientId,
+    required this.ingredientName,
+    required this.quantity,
+    required this.unitId,
+    required this.sourceRecipeIds,
+    required this.shoppingListId,
+    required this.shoppingItemId,
+    required this.pantryTransactionId,
+    required this.pantryLotIds,
+  }) : sourceRecipeIds = List<String>.unmodifiable(sourceRecipeIds),
+       pantryLotIds = List<String>.unmodifiable(pantryLotIds);
+
+  final int metadataVersion;
+  final String id;
+  final DateTime purchasedAt;
+  final String ingredientId;
+  final String canonicalIngredientId;
+  final String ingredientName;
+  final double quantity;
+  final String unitId;
+  final List<String> sourceRecipeIds;
+  final String shoppingListId;
+  final String shoppingItemId;
+  final String pantryTransactionId;
+  final List<String> pantryLotIds;
+}
+
+const int currentPurchaseHistoryEntryVersion = 1;
